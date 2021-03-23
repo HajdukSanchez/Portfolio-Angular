7db847c967159a12770ea7970b1a5e728b50a1c2
Jozek Hajduk <jozekhs@gmail.com>
2021-03-23 11:11:15 -0500

    Finsihed section 3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { ItemComponent } from './pages/item/item.component';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: PortfolioComponent, },
+  { path: 'about', component: AboutComponent, },
+  { path: 'item', component: ItemComponent, },
+  // Esto redirecciona a cualquier otra ruta que no sean alguna de las anteriores, en este caso se redirecciona al principal
+  { path: '**', pathMatch: 'full', redirectTo: '/home' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
